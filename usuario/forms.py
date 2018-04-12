585afa9a81fc8c3fc492fff95f98c173dc31260c
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-04-12 11:07:16 -0500

    Primeras CBV

---
+++ usuario/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from usuario.models import Usuario
+
+class UsuarioForm(forms.ModelForm):
--- usuario/views.py
+++ usuario/views.py
@@ -2,9 +2,34 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+
+from usuario.models import Usuario
+from usuario.forms import UsuarioForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'usuario/index.html')
 
+class UsuarioListView(ListView):
+    model = Usuario
+    template_name = "/usuario/usuario_list.html"
+
+class UsuarioCreateView(CreateView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = "/usuario/usuario_form.html"
+    succes_url = reverse_lazy('usuario:usuario_listar')
+
+class UsuarioUpdateView(UpdateView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = "/usuario/usuario_form.html"
+    succes_url = reverse_lazy('usuario:usuario_listar')
+
+class UsuarioDeleteView(DeleteView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = "/usuario/usuario_delete.html"
+    succes_url = reverse_lazy('usuario:usuario_listar')
