5d3a4e962c0ac0802b309c00ade0c8dadbbb7573
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-04-16 03:20:18 -0500

    se completa modelo de usuario y se añaden tablas al django admin

--- usuario/admin.py
+++ usuario/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Usuario, Rol, Semestre, User_Rol
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Rol)
+admin.site.register(User_Rol)
+admin.site.register(Semestre)
--- usuario/models.py
+++ usuario/models.py
@@ -9,4 +9,67 @@ class Usuario(models.Model):
     username = models.CharField(max_length=20)
     nombre = models.CharField(max_length=60)
     apellidos = models.CharField(max_length=60)
-    rol = models.CharField(max_length=60)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    roles = models.ManyToManyField('self', symmetrical=False, blank=True, through='User_Rol' , related_name='user_roles')
+    estado = models.BooleanField(default=True)
+
+    def delete(self):
+        if self.estado:
+            self.estado = False
+            self.save()
+            return True
+        else:
+            return False
+
+    def __unicode__(self):
+        return '{} {}'.format(self.nombre, self.apellidos)
+
+class Rol(models.Model):
+    nombre = models.CharField(max_length=20)
+    estado = models.BooleanField(default=True)
+
+    def delete(self):
+        if self.estado:
+            self.estado = False
+            self.save()
+            return True
+        else:
+            return False
+
+    def __unicode__(self):
+        return '{}'.format(self.nombre)
+
+class Semestre(models.Model):
+    nombre = models.CharField(max_length=10)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    estado = models.BooleanField(default=True)
+
+    def delete(self):
+        if self.estado:
+            self.estado = False
+            self.save()
+            return True
+        else:
+            return False
+
+    def __unicode__(self):
+        return '{}'.format(self.nombre)
+
+class User_Rol(models.Model):
+    usuario = models.ForeignKey(Usuario)
+    rol = models.ForeignKey(Rol)
+    semestre = models.ForeignKey(Semestre)
+    estado = models.BooleanField(default=True)
+
+    def delete(self):
+        if self.estado:
+            self.estado = False
+            self.save()
+            return True
+        else:
+            return False
+
+    def __unicode__(self):
+        return '{} {} {} {} {} {}'.format(self.usuario.nombre,self.usuario.apellidos,'-', self.rol.nombre,'-', self.semestre.nombre)
