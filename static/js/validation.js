250ffbda561d61c575ebb6064d566559a2a0bdb9
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-04-22 05:00:29 -0500

    primera validacion de horas y creditos. Falta poner Swal

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,17 +1,14 @@
 $(document).on('blur','#horas_magistral', function () {
-    valor = $('#horas_magistral').val();
-    if(valor <= 0){
+    h_magistral = $('#horas_magistral').val();
+    if(h_magistral <= 0){
         alert('Número de horas no válidas. Deben ser mayores a 0')
     }
 
-    valor2 = $('#horas_independientes').val();
-    valor3 = $('#creditos').val();
+    h_independiente = $('#horas_independientes').val();
+    creditos = $('#creditos').val();
 
-    if(valor3 != '' && valor2 != ''){
-        if((Number(valor)+Number(valor2)) != (valor3*3)){
-            console.log(valor+valor2)
-            console.log("")
-            console.log(valor3*3)
+    if(creditos != '' && h_independiente != ''){
+        if((Number(h_magistral)+Number(h_independiente)) != (creditos*3)){
             alert('Número de horas de clase magistral invalidas.')
         }
     }
@@ -19,19 +16,16 @@ $(document).on('blur','#horas_magistral', function () {
 });
 
 $(document).on('blur','#horas_independientes', function () {
-    valor = $('#horas_independientes').val();
-    if(valor <= 0){
+    h_independiente = $('#horas_independientes').val();
+    if(h_independiente <= 0){
         alert('Número de horas no válidas. Deben ser mayores a 0')
     }
 
-    valor2 = $('#horas_magistral').val();
-    valor3 = $('#creditos').val();
+    h_magistral = $('#horas_magistral').val();
+    creditos = $('#creditos').val();
 
-    if(valor3 != '' && valor2 != ''){
-        if((Number(valor)+Number(valor2)) != (valor3*3)){
-            console.log(valor+valor2)
-            console.log("")
-            console.log(valor3*3)
+    if(creditos != '' && h_magistral != ''){
+        if((Number(h_independiente)+Number(h_magistral)) != (creditos*3)){
             alert('Número de horas de clase magistral invalidas.')
         }
     }
@@ -45,14 +39,11 @@ $(document).on('blur','#creditos', function () {
         alert('Número de horas no válidas. Deben ser mayores a 0')
     }
 
-    valor2 = $('#horas_magistral').val();
-    valor3 = $('#horas_independientes').val();
+    h_magistral = $('#horas_magistral').val();
+    h_independiente = $('#horas_independientes').val();
 
-    if(valor3 != '' && valor2 != ''){
-        if((Number(valor3)+Number(valor2)) != (valor*3)){
-            console.log(valor+valor2)
-            console.log("")
-            console.log(valor3*3)
+    if(h_independiente != '' && h-magistral != ''){
+        if((Number(h_independiente)+Number(h-magistral)) != (valor*3)){
             alert('Número de horas de clase magistral invalidas.')
         }
     }
