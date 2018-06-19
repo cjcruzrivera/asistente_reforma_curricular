5a0b429f9b34b2ca1ea2816abff750a667c472bc
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-06-19 15:38:36 -0500

    validaciones curso

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,7 +1,15 @@
 $(document).on('blur','#horas_magistral', function () {
     h_magistral = $('#horas_magistral').val();
     if(h_magistral <= 0){
-        alert('Número de horas no válidas. Deben ser mayores a 0')
+        swal({
+            title: "Error al registrar horas de clase magistrales",
+            text: 'Número de horas no válidas. Deben ser mayores a 0',
+            html: true,
+            icon: "warning",
+            type: "warning",
+        })
+        $('#horas_magistral').val('');
+        return
     }
 
     h_independiente = $('#horas_independientes').val();
@@ -9,7 +17,13 @@ $(document).on('blur','#horas_magistral', function () {
 
     if(creditos != '' && h_independiente != ''){
         if((Number(h_magistral)+Number(h_independiente)) != (creditos*3)){
-            alert('Número de horas de clase magistral invalidas.')
+            swal({
+                title: "Error al registrar horas magistrales",
+                text: "Recuerde que el numero de horas de clase magistrales sumadas a las horas de estudio individual deben dar 3 veces el numero de creditos.",
+                icon: "warning",
+                buttons: false,
+                dangerMode: true,
+              })
         }
     }
 
@@ -18,7 +32,15 @@ $(document).on('blur','#horas_magistral', function () {
 $(document).on('blur','#horas_independientes', function () {
     h_independiente = $('#horas_independientes').val();
     if(h_independiente <= 0){
-        alert('Número de horas no válidas. Deben ser mayores a 0')
+        swal({
+            title: "Error al registrar horas de estudio independiente",
+            text: 'Número de horas no válidas. Deben ser mayores a 0',
+            html: true,
+            icon: "warning",
+            type: "warning",
+        })
+        $('#horas_independientes').val('');
+        return
     }
 
     h_magistral = $('#horas_magistral').val();
@@ -26,7 +48,15 @@ $(document).on('blur','#horas_independientes', function () {
 
     if(creditos != '' && h_magistral != ''){
         if((Number(h_independiente)+Number(h_magistral)) != (creditos*3)){
-            alert('Número de horas de clase magistral invalidas.')
+            swal({
+                title: "Error al registrar horas de estudio independiente",
+                text: "Recuerde que el numero de horas de clase magistrales sumadas a las horas de estudio individual deben dar 3 veces el numero de creditos.",
+                html: true,
+                icon: "warning",
+                type: "warning",
+            })
+            $('#horas_independientes').val('');
+            return
         }
     }
 
@@ -36,7 +66,15 @@ $(document).on('blur','#horas_independientes', function () {
 $(document).on('blur','#creditos', function () {
     valor = $('#creditos').val();
     if(valor <= 0){
-        alert('Número de horas no válidas. Deben ser mayores a 0')
+        swal({
+            title: "Error al registrar numero de creditos",
+            text: "Debe ser mayor a 0",
+            html: true,
+            icon: "warning",
+            type: "warning",
+        })
+        $('#creditos').val('');
+        return
     }
 
     h_magistral = $('#horas_magistral').val();
@@ -44,7 +82,16 @@ $(document).on('blur','#creditos', function () {
 
     if(h_independiente != '' && h-magistral != ''){
         if((Number(h_independiente)+Number(h-magistral)) != (valor*3)){
-            alert('Número de horas de clase magistral invalidas.')
+            swal({
+                title: "Error al registrar creditos",
+                text: "Recuerde que el numero de horas de clase magistrales sumadas a las horas de estudio individual deben dar 3 veces el numero de creditos.",
+                html: true,
+                icon: "warning",
+                type: "warning",
+            })
+            $('#creditos').val('');
+            return
+
         }
     }
 
