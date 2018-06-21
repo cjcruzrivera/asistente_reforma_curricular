3b9fb427569ca2630cae4723454b3a5640cfa582
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-06-21 13:37:52 -0500

    actividad en resultado

--- resultado_aprendizaje/urls.py
+++ resultado_aprendizaje/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from .views import ResultadoAprendizajeCreateView, ResultadoAprendizajeUpdateView, eliminar, view_one
+from .views import ResultadoAprendizajeCreateView, ResultadoAprendizajeUpdateView, eliminar, view_one, actividad
 urlpatterns = [
     url(r'^nuevo/(?P<pk>\d+)/$', login_required(ResultadoAprendizajeCreateView.as_view()), name='resultadoAprendizaje_crear'),
     url(r'^editar/(?P<pk>\d+)/$', login_required(ResultadoAprendizajeUpdateView.as_view()), name='resultadoAprendizaje_editar'),
     url(r'^eliminar/$', login_required(eliminar), name='eliminar'),
     url(r'^view/(?P<pk>\d+)/$', login_required(view_one), name='view_one'),
+    url(r'^actividad/$', login_required(actividad), name='actividad'),
 
 ]
---
+++ static/js/resultados_view.js
@@ -0,0 +1,163 @@
+$(document).ready(function() {
+    $('#indicadores').DataTable({
+        "language": {
+            "lengthMenu": "Mostrando _MENU_ programas academicos",
+            "info": "Página _PAGE_ de _PAGES_",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primera",
+                "previous": "Pág. Anterior",
+                "next": "Siguiente",
+                "last": "Última"
+            }
+        }
+    });
+
+    $('#actividades').DataTable({
+        "language": {
+            "lengthMenu": "Mostrando _MENU_ programas academicos",
+            "info": "Página _PAGE_ de _PAGES_",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primera",
+                "previous": "Pág. Anterior",
+                "next": "Siguiente",
+                "last": "Última"
+            }
+        }
+    });
+
+});
+
+$(document).on('click','#editar', function () {
+    var tipo_act = $('#tipo').val();
+    if(tipo_act == 0){
+        swal({
+            title: "Seleccione el tipo de actividad",
+            text: '',
+            html: true,
+            icon:'warning',
+            type: "warning",
+            confirmButtonColor: "#d51b23"
+        });
+        return
+    }
+
+    var descripcion_act = $('#descripcion').val();
+    if(descripcion == ''){
+        swal({
+            title: "Ingrese la descripcion de la actividad",
+            text: '',
+            html: true,
+            icon:'warning',
+            type: "warning",
+            confirmButtonColor: "#d51b23"
+        });
+        return
+    }
+
+    var actividad = $(this).attr('data-id');
+    $.ajax({
+        type: "POST",
+        data: {
+            id_actividad: actividad,
+            tipo: tipo_act,
+            descripcion: descripcion_act,
+        },
+        url: "/resultado/actividad/",
+        success: function (msg) {
+            swal({
+                title: "Almacenado con éxito",
+                text: "La actividad ha sido registrada correctamente",
+                icon: "success",
+                buttons: false,
+                timer: 1500,
+            });
+            setTimeout(function () { location.reload(true); }, 1500);
+
+        },
+        async: false,
+        dataType: "json",
+        cache: "false",
+        error: function (msg) {
+            swal({
+                title: "Error AJAX",
+                text: msg.responseText,
+                html: true,
+                type: "warning",
+                confirmButtonColor: "#d51b23"
+            });
+            console.log("AJAXerror");
+            console.log(msg);
+
+        },
+    });
+
+
+})
+
+$(document).on('click', '#guardar', function () {
+    var tipo_act = $('#tipo').val();
+    if(tipo_act == 0){
+        swal({
+            title: "Seleccione el tipo de actividad",
+            text: '',
+            html: true,
+            icon:'warning',
+            type: "warning",
+            confirmButtonColor: "#d51b23"
+        });
+        return
+    }
+
+    var descripcion_act = $('#descripcion').val();
+    if(descripcion == ''){
+        swal({
+            title: "Ingrese la descripcion de la actividad",
+            text: '',
+            html: true,
+            icon:'warning',
+            type: "warning",
+            confirmButtonColor: "#d51b23"
+        });
+        return
+    }
+
+    var resultado = $(this).attr('data-resultado')
+
+    $.ajax({
+        type: "POST",
+        data: {
+            id_resultado: resultado,
+            tipo: tipo_act,
+            descripcion: descripcion_act,
+        },
+        url: "/resultado/actividad/",
+        success: function (msg) {
+            swal({
+                title: "Almacenado con éxito",
+                text: "La actividad ha sido registrada correctamente",
+                icon: "success",
+                buttons: false,
+                timer: 1500,
+            });
+            setTimeout(function () { location.reload(true); }, 1500);
+
+        },
+        async: false,
+        dataType: "json",
+        cache: "false",
+        error: function (msg) {
+            swal({
+                title: "Error AJAX",
+                text: msg.responseText,
+                html: true,
+                type: "warning",
+                confirmButtonColor: "#d51b23"
+            });
+            console.log("AJAXerror");
+            console.log(msg);
+
+        },
+    });
+})
